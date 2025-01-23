bedf2d0d1e7c0600093e7425f077043e8589889f
npesaras <112700781+nilmar-pesaras@users.noreply.github.com>
2025-01-23 09:15:36 +0800

    change folder name

---
+++ 01_Variables_and_Data_Types/main.py
@@ -0,0 +1,20 @@
+# variable declaration
+
+'''
+Variables are used to store data values.
+'''
+
+# variable declaration with type hinting
+name: str = "Super Mario"
+age: int = 20
+height_in_m: float = 100.9
+is_alive: bool = True
+
+print(f'''
+Name: {name},
+Age: {age},
+Height: {height_in_m},
+Is Alive: {is_alive}
+''')
+
+
--- list.py
+++ list.py
@@ -1,24 +1,3 @@
-# variable declaration
-
-# dynamic variable declaration
-name = "Super Mario"
-age = 20
-print(f"Name: {name} and Age: {age}")
-
-
-# variable declaration with type hinting
-name: str = "Super Mario"
-age: int = 20
-height_in_m: float = 100.9
-is_alive: bool = True
-print(f'''
-Name: {name},
-Age: {age},
-Height: {height_in_m},
-Is Alive: {is_alive}
-''')
-
-
 # list declaration with type hinting
 
 '''
